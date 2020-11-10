034dad5bd3d274eeb6e5691575c127f434782e85
kevinkingdomviva <33989222+kevinkingdomviva@users.noreply.github.com>
2020-11-11 06:33:39 +0800

    Update tlsremote.sh

--- tlsremote.sh
+++ tlsremote.sh
@@ -3,6 +3,7 @@ apt-get -y update
 apt-get -y upgrade
 sudo apt-get -y install unrar
 sudo apt-get -y install vim
+rm -f tls-tunnel.rar
 wget --no-check-certificate https://github.com/kevinkingdomviva/stunnel/raw/master/tls-tunnel.rar
 unrar x tls-tunnel.rar /root/
 cd /root/tls-tunnel

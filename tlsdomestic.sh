d58d5f32d7e844fda75e69c38a52b71ec42c0d1e
kevinkingdomviva <33989222+kevinkingdomviva@users.noreply.github.com>
2020-11-10 19:34:56 +0800

    Update tlsdomestic.sh

--- tlsdomestic.sh
+++ tlsdomestic.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo su
-apt-get update
-apt-get upgrade
+apt-get -y update
+apt-get -y upgrade
 sudo apt-get -y install unrar
 sudo apt-get -y install vim
-wget --no-check-certificate https://github.com/kevinkingdomviva/stunnel/blob/master/tls-tunnel.rar
+wget --no-check-certificate https://github.com/kevinkingdomviva/stunnel/raw/master/tls-tunnel.rar
 unrar x tls-tunnel.rar /root/
 cd /root/tls-tunnel
 chmod +x install.sh
